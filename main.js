fa1e3082e0a6ab6bc6615c754c8bc13acd0d94d5
AARAV2909 <78532763+AARAV2909@users.noreply.github.com>
2022-02-11 20:22:16 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+Justin_Bieber_Theme_song = "";
+Harry_Potter_Theme_song = "";
+
+function setup(){
+    caanvas = createCanvas(600, 530);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+function draw(){
+    image(video, 0, 0, 600, 530);
+}
+
+function preload(){
+    Harry_Potter_Theme_song = loadSound("music.mp3");
+    Justin_Bieber_Theme_song = loadSound("music.mp2");
+}
